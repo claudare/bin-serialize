c6b7a664dcb4adde832579cc112815f03cea0dc5
Roman <roman@volovoy.com>
2025-01-15 12:35:15 +0100

    removed exe, now lib-only build.zig

--- src/config.zig
+++ src/config.zig
@@ -6,6 +6,8 @@ pub const ConfigRuntime = struct {
     len: usize,
 };
 
+// these must be placed inside the configuration of the library, so they are defined at build time
+// and that is a requirement just because the "type" used in encoding slices must be compiletime known...
 pub const ConfigSerialization = struct {
     endian: std.builtin.Endian = .big,
     slice_len_bitsize: u16 = 32, // by default u32 is used to serialize arrays
@@ -16,12 +18,12 @@ pub const ConfigSerialization = struct {
         if (@rem(self.slice_len_bitsize, 8) != 0) {
             @compileError("slice encoding type must be divisible by 8. Instead got " ++ self.sliceLenSize());
         }
-
-        return comptime @Type(.{
-            .Int = .{
-                .bits = self.slice_len_bitsize,
-                .signedness = .unsigned,
-            },
-        });
+        return std.meta.Int(.unsigned, self.slice_len_bitsize);
+        // return @Type(.{
+        //     .Int = .{
+        //         .bits = self.slice_len_bitsize,
+        //         .signedness = .unsigned,
+        //     },
+        // });
     }
 };
